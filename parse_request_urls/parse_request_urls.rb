56113c1e48a01571c0e1f649f4bf1a2d133d5f80
Phil McGachey <phil_mcgachey@harvard.edu>
2015-07-13 14:10:35 -0400

    Changed parse_request_urls to work day-by-day

--- parse_request_urls/parse_request_urls.rb
+++ parse_request_urls/parse_request_urls.rb
@@ -1,27 +1,34 @@
 require 'json'
 
-Dir.foreach('/mnt/data/data/') do |filename|
-  next unless filename =~ /\Arequests.*\.csv\z/
-  puts "Processing: #{filename}"
+filename = ARGV[0]
+puts "Processing: #{filename}"
 
-  requests = CSV.read("/mnt/data/data/#{filename}")
-  bad_routes = []
-  File.open("/mnt/data/data/#{filename}.paths", 'w') do |f|
-    requests.each do |request|
+bad_routes = []
+
+File.open("#{filename}.paths", 'w') do |f|
+  File.open("#{filename}.errors", 'w') do |errors|
+    CSV.foreach(filename) do |request|
       url = request[11]
       verb = request[16]
       begin
         route_info = Rails.application.routes.recognize_path url, method: verb
         f.write "#{request[0]},#{JSON.dump(route_info)}\n"
-      rescue
+      rescue ActionController::RoutingError
         bad_routes << request
+      rescue => e
+        bad_routes << request
+        errors.write "Error processings #{verb} #{url}"
+        errors.write e.message
+        errors.write e.backtrace.join("\n")
+        errors.write "\n\n"
       end
     end
   end
+end
 
-  File.open("/mnt/data/data/#{filename}.bad_routes", 'w') do |f|
-    bad_routes.each do |bad_route|
-      f.write "#{bad_route[16]} #{bad_route[11]}\n"
-    end
+File.open("#{filename}.bad_routes", 'w') do |f|
+  bad_routes.each do |bad_route|
+    f.write "#{bad_route[16]} #{bad_route[11]}\n"
   end
 end
+puts "Done with #{filename}"
